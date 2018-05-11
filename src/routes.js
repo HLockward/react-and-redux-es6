b30ef2ee38133a9825161b7b1a021488420169e5
hlockward <hlockward12@gmail.com>
2018-05-10 22:32:55 -0400

    manager course page

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursePage from './components/course/CoursesPage';
+import ManagerCoursePage from './components/course/ManagerCoursePage';
 
 export default(
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="about" component={AboutPage}/>
     <Route path="courses" component={CoursePage}/>
+    <Route path="course" component={ManagerCoursePage}/>
+    <Route path="course/:id" component={ManagerCoursePage}/>
   </Route>
 );
